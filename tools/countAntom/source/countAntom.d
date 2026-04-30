a6101f9d3ea2697126b713277ab3e95a3aed3ba2
wee0123 <1505506712@qq.com>
2026-04-30 11:35:43 +0800

    1.0

---
+++ tools/countAntom/source/countAntom.d
@@ -0,0 +1,9 @@
+source/countAntom.o: source/countAntom.cpp include/core.hpp \
+ include/rss.hpp include/countAntom.hpp include/clause.hpp \
+ include/component.hpp include/countAntom.hpp
+include/core.hpp:
+include/rss.hpp:
+include/countAntom.hpp:
+include/clause.hpp:
+include/component.hpp:
+include/countAntom.hpp:
---
+++ tools/countAntom/source/main.d
@@ -0,0 +1,2 @@
+source/main.o: source/main.cpp include/countAntom.hpp
+include/countAntom.hpp:
